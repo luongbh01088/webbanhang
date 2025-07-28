696056ec9fb5b589d0fe8448a33565ab4ce003a5
luongbh01088 <luongtvdbh01088>
2025-07-28 08:10:05 +0700

    Initial commit

---
+++ pages/index.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>WEb bán hàng </title>
+</head>
+<body>
+    <div class="wrapper">
+        <div class ="header">
+           <h1>Welcome to ShoeZone</h1>
+    <p>Discover the latest trends in footwear. Shop now for the best deals on sneakers, boots, sandals, and more!</p>
+    </div>
+</body>
+</html>
